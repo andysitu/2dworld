da3c819d1f7bd799a08ae09294bec04669043c39
Andy <and.situ@gmail.com>
2015-07-13 11:25:45 -0700

    Player can move now

--- world.js
+++ world.js
@@ -8,21 +8,32 @@ var world = {
 			map.appendChild(tr);
 			for (var j = 0; j < maps[i].length; j++) {
 				var th = document.createElement("th");
+				if (maps[i][j] == " ") {
+					th.setAttribute("class", "space");
+				} else if (maps[i][j] == "P") {
+					th.setAttribute("class", "player");
+					this.playerLoc = [i, j];
+				}
+				th.setAttribute("id", i + "" + j);
 				tr.appendChild(th);
 			}
-
 		}
 	},
 
-	displayMap(str) {
-		var mapLoc = document.getElementById("map");
-		mapLoc.innerHTML = str;
+	changeClass(class1, x1, y1) {
+		var loc = document.getElementById(x1 + "" + y1);
+		loc.setAttribute("class", class1);
 	},
 
 	move(arr, y, x, y1, x1) {
-		if (arr[y1][x1] == "&nbsp") {
-			arr[y1][x1] = arr[y][x];
-			arr[y][x] = "&nbsp";
+		if (arr[y1][x1] == " ") {
+			arr[y1][x1] = arr[y][x]; // set new location of player
+			this.changeClass("player", y1, x1);
+
+			arr[y][x] = " "; // set area where player was to "space"
+			this.changeClass("space", y, x);
+
+			this.playerLoc = [y1, x1]; // change the player coordinates record
 		}
 	},
 };
