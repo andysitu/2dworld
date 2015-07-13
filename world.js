81dd7c21d2842cce09c4d90bd5d3463065c8fe81
Andy <and.situ@gmail.com>
2015-07-12 17:40:40 -0700

    rudimentary player(P) movement

--- world.js
+++ world.js
@@ -14,12 +14,18 @@ var map =
 		["&nbsp", "&nbsp", "&nbsp", "&nbsp", "&nbsp", "&nbsp", "&nbsp", "f"] ];
 
 var world = {
+	playerLoc: [0,0],
 	translateMap(maps) {
 		var string = "";
 
 		for (var i = 0; i < maps.length; i++) {
 			for (var j = 0; j < maps[i].length; j++) {
-				string += maps[i][j];
+				if (maps[i][j] == "P") {
+					this.playerLoc = [i, j];
+					string += maps[i][j];
+				} else {
+					string += maps[i][j];
+				}
 			}
 			string += "\n"
 		}
@@ -31,8 +37,30 @@ var world = {
 		var mapLoc = document.getElementById("map");
 		mapLoc.innerHTML = str;
 	},
+
+	move(arr, y, x, y1, x1) {
+		if (arr[y1][x1] == "&nbsp") {
+			arr[y1][x1] = arr[y][x];
+			arr[y][x] = "&nbsp";
+		}
+	},
 };
 
 window.onload = function() {
 	world.displayMap(world.translateMap(map));
+	var mapID = document.getElementById("map");
+
+	document.onkeydown = function(e) {
+		if (e.keyCode == 37) {
+			world.move(map, world.playerLoc[0], world.playerLoc[1], world.playerLoc[0], world.playerLoc[1] - 1);
+			world.displayMap(world.translateMap(map));
+		} else if (e.keyCode == 38) {
+			world.move(map, world.playerLoc[0], world.playerLoc[1], world.playerLoc[0] - 1, world.playerLoc[1]);
+			world.displayMap(world.translateMap(map));
+		} else if (e.keyCode == 39) {
+			world.move(map, world.playerLoc[0], world.playerLoc[1], world.playerLoc[0], world.playerLoc[1] + 1);
+			world.displayMap(world.translateMap(map));
+		} else if (e.keyCode == 40) {
+			world.move(map, world.playerLoc[0], world.playerLoc[1], world.playerLoc[0] + 1, world.playerLoc[1]);
+			world.displayMap(world.translateMap(map));
 };
