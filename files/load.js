1bc919e91acf88211810f9a6b93a58ef0e417d60
Andy Situ <and.situ@gmail.com>
2015-08-31 21:36:09 -0700

    add main function to onload

--- files/load.js
+++ files/load.js
@@ -1,9 +1,27 @@
+var canvas,
+	ctx;
+
 window.addEventListener("load", function() {
-	var canvas = document.createElement("canvas"),
-		ctx = canvas.getContext('2d');
+	canvas = document.createElement("canvas");
+	ctx = canvas.getContext('2d');
 
 	canvas.width = 1000;
-	canvas.height = 800;
+	canvas.height = 600;
 	var cont = document.getElementById("canvas_container");
 	cont.appendChild(canvas);
+
+	function main() {
+		var now = Date.now();
+		var modifier = (now - past) / 1000;
+
+		player.update(modifier);
+		player.draw(ctx);
+
+		past = now;
+
+		requestAnimationFrame(main);
+	}
+
+	var past = Date.now();
+	main();
 })
